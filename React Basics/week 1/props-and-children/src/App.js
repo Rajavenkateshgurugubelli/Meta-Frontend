25f4ce20a77e7211489d6ec7a369d1dbe8969ba9
Rajavenkateshgurugubelli <120995535+Rajavenkateshgurugubelli@users.noreply.github.com>
2024-02-17 17:31:19 -0600

    props and children

---
+++ React Basics/week 1/props-and-children/src/App.js
@@ -0,0 +1,22 @@
+import Bag from "./Bag";
+import Apples from "./Apples";
+import Pears from "./Pears";
+
+function App() {
+  return (
+    <div className="App">
+      <Bag children={<Apples color="yellow" number="5" />} />
+      <Bag children={<Pears friend="Peter" />} />
+      <Bag>
+        <Apples color="yellow" number="5" />
+      </Bag>
+
+      <Bag>
+        <Pears friend="Peter" />
+      </Bag>
+
+    </div>
+  );
+};
+
+export default App;
