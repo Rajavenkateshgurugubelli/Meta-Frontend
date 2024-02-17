fc58c2cb90220f050266aed8ea80b9646b8b170d
Rajavenkateshgurugubelli <120995535+Rajavenkateshgurugubelli@users.noreply.github.com>
2024-02-17 16:12:18 -0600

    passing props

---
+++ React Basics/week 1/Passing props/passing-props/src/App.js
@@ -0,0 +1,11 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName='Raja' />
+        </div>
+    );
+};
+
+export default App;
---
+++ React Basics/week 1/Passing props/passing-props/src/Heading.js
@@ -0,0 +1,7 @@
+function Heading(Props) {
+  return (
+      <h1>Hello,{Props.firstName} </h1>
+  )
+}
+
+export default Heading;
